4129c3ef8e6943815e7f7f34dff3977f18caa621
mlibera <mateusz.libera@capgemini.com>
2023-04-24 13:59:23 +0200

    initial commit

---
+++ src/main/kotlin/com/example/jenkinsDemo/JenkinsDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.jenkinsDemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JenkinsDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<JenkinsDemoApplication>(*args)
+}
