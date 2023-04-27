7365496666e589578069e16e4e7051ca65c36951
mlibera <mateusz.libera@capgemini.com>
2023-04-27 13:19:22 +0200

    User Model update and Test for test

--- src/main/kotlin/com/example/jenkinsDemo/model/User.kt
+++ src/main/kotlin/com/example/jenkinsDemo/model/User.kt
@@ -1,8 +1,20 @@
 package com.example.jenkinsDemo.model
 
+import lombok.Getter
+import lombok.Setter
+import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.persistence.Table
 
-data class User(@Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long, val name: String, val email: String, val password: String ) {
+@Entity
+@Table(name = "User")
+@Getter
+@Setter
+data class User(@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+                var id: Long,
+                val name: String,
+                val email: String,
+                val password: String){
 }
--- src/test/kotlin/com/example/jenkinsDemo/JenkinsDemoApplicationTests.kt
+++ src/test/kotlin/com/example/jenkinsDemo/JenkinsDemoApplicationTests.kt
@@ -2,8 +2,17 @@ package com.example.jenkinsDemo
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.util.Assert
 
 @SpringBootTest
 class JenkinsDemoApplicationTests {
 
+    @Test
+    fun contextLoads() {
+    }
+    @Test
+    fun test(){
+        val test = "test"
+        Assert.isTrue(test.equals("test"))
+    }
 }
